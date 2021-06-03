88fc9a8fb23d069dfd852ab8cb581f23cce000ba
clem-hml <83707944+clem-hml@users.noreply.github.com>
2021-06-03 18:54:35 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,10 @@
 
+# On notera :
+#      " b_ " pour l'affichage de boutons
+#      " l_ " ou " l " pour l'affichage de labels (messages)
+
+
+
 #--------------Import---------------------------------
 
 import tkinter as tk
@@ -6,71 +12,26 @@ from tkinter import filedialog as fd
 import os.path
 from tkinter.filedialog import askopenfilename
 
-#----------Création de la bar de menu-----------------
 
+
+#----------CREATION BARRE DE MENU-----------------
 
 def bar_menu():
-    menubar = tk.Menu()
+    menu_bar = tk.Menu()
 
     def openFile():
         file = askopenfilename(title="Choisir un fichier à ouvrir")
         filetypes = [("PNG image", ".png"), ("GIF image", ".gif"), ("All files", ".*")]
         print(file)
 
-    menu1 = tk.Menu(menubar, tearoff=0)
-    menubar.add_command(label="Ouvrir", command=openFile)
+    menu1 = tk.Menu(menu_bar, tearoff=0)
+    menu_bar.add_command(label="Ouvrir", command=openFile)
     menu1.add_separator()
-    menubar.add_command(label="Quitter", command=quit)
-    root.config(menu=menubar)
+    menu_bar.add_command(label="Quitter", command=quit)
+    root.config(menu=menu_bar)
 
 
-#-----------Menu----------------------
-
-def scale1():
-    lobby_scale = tk.Tk()
-    lobby_scale.title("Caractéristiques")
-    lobby_scale.geometry('500x300')
-    lobby_scale.config(background="#AED6F1")
-
-    l = tk.Label(lobby_scale, bg='#D6EAF8', fg='black', font=("Courrier", 15), text='Puissance')
-    l.place(relx=0.300, rely=0.050, width=200, height=50)
-
-    def print_selection(v):
-        l.config(text='Vos dégâts : ' + v)
-
-    s = tk.Scale(lobby_scale, label='Bougez le joystick', from_=0, to=10, orient=tk.HORIZONTAL, length=400, showvalue=0, tickinterval=2,
-                 resolution=0.01, command=print_selection)
-    s.place(relx=0.100, rely=0.300)
-
-    bouton8 = tk.Button(lobby_scale, text="Enregistrer", width=10, font="Courrier", bg='#5499C7', fg='white')
-    bouton8.place(relx=0.380, rely=0.700)
-
-    lobby_scale.mainloop()
-
-
-def menu_objet():
-    lobby_objet = tk.Tk()
-    lobby_objet.title("Lobby objet")
-    lobby_objet.minsize(1000, 750)
-    lobby_objet.config(background="#A3E4D7")
-
-    texte3 = tk.Label(lobby_objet,text="Créez vos objets \n",font=("Courrier", 15), bg="#D1F2EB", fg="black")
-    texte3.place(relx=0.360, rely=0.050, width=300, height=50)
-
-    texte = tk.Text(lobby_objet, font=("Courrier", 15), bg="#AED6F1", fg="black", width=7, height=1)
-    texte.place(relx=0.350, rely=0.280, width=300, height=40)
-
-    texte4 = tk.Label(lobby_objet, text="Nom de l'objet :\n", font=("Courrier", 15), bg="#D1F2EB", fg="black")
-    texte4.place(relx=0.100, rely=0.280, width=200, height=40)
-
-    bouton6 = tk.Button(lobby_objet, text="Enregistrer", width=10, font=("Courrier", 15), bg='#16A085', fg='white')
-    bouton6.place(relx=0.700, rely=0.280, width=150, height=45)
-
-    bouton6bis = tk.Button(lobby_objet, text="Retour", width=10, font="Courrier", bg='white', fg='red', command=lambda: [lobby_objet.destroy()])
-    bouton6bis.place(relx=0.870, rely=0.010)
-
-    bouton7 = tk.Button(lobby_objet, text="Caractéristiques de l'objet", font=("Courrier", 18), bg='#16A085', fg='white', command=lambda: [lobby_objet.quit, scale1()])
-    bouton7.place(relx=0.100, rely=0.480, width=400, height=50)
+#-----------CREATION PAGE EDITION----------------------
 
 def menu_edition():
     lobby_edition = tk.Tk()
@@ -78,19 +39,22 @@ def menu_edition():
     lobby_edition.minsize(1000, 650)
     lobby_edition.config(background="#A3E4D7")
 
-    texte5 = tk.Label(lobby_edition, text="Que voulez-vous faire ?", font=("Courrier", 20), bg="#D1F2EB", fg="black")
-    texte5.place(relx=0.330, rely=0.050, width=350, height=80)
+    l1 = tk.Label(lobby_edition, text="Que voulez-vous faire ?", font=("Courrier", 20), bg="#D1F2EB", fg="black")
+    l1.place(relx=0.330, rely=0.050, relwidth=0.35, relheight=0.08)
 
-    bouton8 = tk.Button(lobby_edition, text="Texte", width=10, font=("Courrier", 20), bg='#16A085', fg='white', command=lambda: [lobby_edition.quit, menu_texte()])
-    bouton8.place(relx=0.350, rely=0.400, width=300, height=50)
+    b_texte = tk.Button(lobby_edition, text="Texte", width=10, font=("Courrier", 20), bg='#16A085', fg='white', command=lambda: [lobby_edition.quit, menu_texte()])
+    b_texte.place(relx=0.350, rely=0.400, relwidth=0.3, relheight=0.08)
 
-    bouton9 = tk.Button(lobby_edition, text="Objet", width=10, font=("Courrier", 20), bg='#16A085', fg='white', command=lambda: [lobby_edition.quit, menu_objet()])
-    bouton9.place(relx=0.350, rely=0.650, width=300, height=50)
+    b_objet = tk.Button(lobby_edition, text="Objet", width=10, font=("Courrier", 20), bg='#16A085', fg='white', command=lambda: [lobby_edition.quit, menu_objet()])
+    b_objet.place(relx=0.350, rely=0.650, relwidth=0.3, relheight=0.08)
 
-    bouton10 = tk.Button(lobby_edition, text="Retour", width=10, font="Courrier", bg='white', fg='red',
-                        command=lambda: [lobby_edition.destroy()])
-    bouton10.place(relx=0.870, rely=0.010)
+    b_retour1 = tk.Button(lobby_edition, text="Retour", width=10, font="Courrier", bg='white', fg='red', command=lambda: [lobby_edition.destroy()])
+    b_retour1.place(relx=0.870, rely=0.010, relwidth=0.12, relheight=0.07)
 
+
+
+
+#-----Edition------CREATION PAGE TEXTE----------------------
 
 def menu_texte():
     lobby_texte = tk.Tk()
@@ -98,41 +62,98 @@ def menu_texte():
     lobby_texte.minsize(1000, 650)
     lobby_texte.config(background="#A3E4D7")
 
-    texte0 = tk.Label(lobby_texte, text="Titre", font=("Courrier", 15), bg="#D1F2EB", fg="black")
-    texte0.place(relx=0.450, rely=0.050, width=100, height=40)
+    l_titre = tk.Label(lobby_texte, text="Titre", font=("Courrier", 15), bg="#D1F2EB", fg="black")
+    l_titre.place(relx=0.450, rely=0.050, relwidth=0.1, relheight=0.07)
 
-    titre0 = tk.Text(lobby_texte,font=("Courrier", 20), bg="#AED6F1", fg="black")
-    titre0.place(relx=0.100, rely=0.150, width=800, height=50)
+    titre = tk.Text(lobby_texte,font=("Courrier", 20), bg="#AED6F1", fg="black")
+    titre.place(relx=0.100, rely=0.150, relwidth=0.8, relheight=0.07)
 
-    texte2 = tk.Label(lobby_texte, text="Texte", font=("Courrier", 15), bg="#D1F2EB", fg="black")
-    texte2.place(relx=0.450, rely=0.250, width=100, height=40)
+    l_texte = tk.Label(lobby_texte, text="Texte", font=("Courrier", 15), bg="#D1F2EB", fg="black")
+    l_texte.place(relx=0.450, rely=0.250, relwidth=0.1, relheight=0.07)
 
-    texte1 = tk.Text(lobby_texte, font=("Courrier", 20), bg="#AED6F1", fg="black")
-    texte1.place(relx=0.100, rely=0.350, width=800, height=330)
+    texte = tk.Text(lobby_texte, font=("Courrier", 20), bg="#AED6F1", fg="black")
+    texte.place(relx=0.100, rely=0.350, relwidth=0.8, relheight=0.5)
+
+    b_retour2 = tk.Button(lobby_texte, text="Retour", width=10, font="Courrier", bg='white', fg='red', command=lambda: [lobby_texte.destroy()])
+    b_retour2.place(relx=0.870, rely=0.010, relwidth=0.12, relheight=0.07)
 
     def save1():
-        texte = os.path.join(f"{titre0.get('1.0', 'end-1c')}.txt")
-        txt = open(texte, "a+")
-        txt.write(texte1.get('1.0', 'end-1c'))
+        save_texte = os.path.join(f"{titre.get('1.0', 'end-1c')}.txt")
+        txt = open(save_texte, "a+")
+        txt.write(texte.get('1.0', 'end-1c'))
         txt.close()
 
-    bouton3 = tk.Button(lobby_texte, text="Enregistrer", width=10, font="Courrier", bg='#16A085', fg='white', command=save1())
-    bouton3.place(relx=0.450, rely=0.900)
+    b_enregistrer1 = tk.Button(lobby_texte, text="Enregistrer", width=10, font="Courrier", bg='#16A085', fg='white', command=save1())
+    b_enregistrer1.place(relx=0.450, rely=0.900, relwidth=0.12, relheight=0.07)
 
-    bouton4 = tk.Button(lobby_texte, text="Retour", width=10, font="Courrier", bg='white', fg='red', command=lambda: [lobby_texte.destroy()])
-    bouton4.place(relx=0.870, rely=0.010)
 
+
+#-----Edition------CREATION PAGE OBJET----------------------
+
+def menu_objet():
+    lobby_objet = tk.Tk()
+    lobby_objet.title("Lobby objet")
+    lobby_objet.minsize(1000, 750)
+    lobby_objet.config(background="#A3E4D7")
+
+    l_objet1 = tk.Label(lobby_objet,text="Créez vos objets \n",font=("Courrier", 15), bg="#D1F2EB", fg="black")
+    l_objet1.place(relx=0.360, rely=0.050, relwidth=0.25, relheight=0.07)
+
+    l_nom = tk.Label(lobby_objet, text="Nom de l'objet :\n", font=("Courrier", 15), bg="#D1F2EB", fg="black")
+    l_nom.place(relx=0.100, rely=0.280, relwidth=0.2, relheight=0.07)
+
+    nom = tk.Text(lobby_objet, font=("Courrier", 15), bg="#AED6F1", fg="black", width=7, height=1)
+    nom.place(relx=0.350, rely=0.280, relwidth=0.25, relheight=0.07)
+
+    b_enregistrer2 = tk.Button(lobby_objet, text="Enregistrer", width=10, font=("Courrier", 15), bg='#16A085', fg='white')
+    b_enregistrer2.place(relx=0.700, rely=0.280, relwidth=0.12, relheight=0.07)
+
+    b_retour3 = tk.Button(lobby_objet, text="Retour", width=10, font="Courrier", bg='white', fg='red', command=lambda: [lobby_objet.destroy()])
+    b_retour3.place(relx=0.870, rely=0.010, relwidth=0.12, relheight=0.07)
+
+    b_cara = tk.Button(lobby_objet, text="Caractéristiques de l'objet", font=("Courrier", 18), bg='#16A085', fg='white', command=lambda: [lobby_objet.quit, scale1()])
+    b_cara.place(relx=0.100, rely=0.480, relwidth=0.3, relheight=0.07)
+
+
+# Création d'une scale pour gérer la puissance de l'objet :
+
+def scale1():
+    lobby_scale = tk.Tk()
+    lobby_scale.title("Caractéristiques")
+    lobby_scale.geometry('500x300')
+    lobby_scale.config(background="#AED6F1")
+
+    l_puissance = tk.Label(lobby_scale, bg='#D6EAF8', fg='black', font=("Courrier", 15), text='Puissance')
+    l_puissance.place(relx=0.350, rely=0.050, relwidth=0.3, relheight=0.1)
+
+
+    def print_selection(v):
+        l_puissance.config(text='Vos dégâts : ' + v)
+
+    scale = tk.Scale(lobby_scale, label='Bougez le joystick', from_=0, to=10, orient=tk.HORIZONTAL, length=400, showvalue=0, tickinterval=2,
+                 resolution=0.01, command=print_selection)
+    scale.place(relx=0.200, rely=0.300, relwidth=0.6, relheight=0.25)
+
+    b_enregistrer3 = tk.Button(lobby_scale, text="Enregistrer", width=10, font="Courrier", bg='#5499C7', fg='white')
+    b_enregistrer3.place(relx=0.350, rely=0.700, relwidth=0.3, relheight=0.1)
+
+    lobby_scale.mainloop()
+
+
+
+#-----Jeu------Création de la page de jeu-----------------------
 
 def menu_jeu():
     lobby_jeu = tk.Tk()
     lobby_jeu.title("Lobby")
     lobby_jeu.minsize(1000, 650)
     lobby_jeu.config(background="#A3E4D7")
-    bouton4bis = tk.Button(lobby_jeu, text="Retour", width=10, font="Courrier", bg='white', fg='red', command=lambda: [lobby_jeu.destroy()])
-    bouton4bis.place(relx=0.870, rely=0.010)
+    b_retour4 = tk.Button(lobby_jeu, text="Retour", width=10, font="Courrier", bg='white', fg='red', command=lambda: [lobby_jeu.destroy()])
+    b_retour4.place(relx=0.870, rely=0.010, relwidth=0.12, relheight=0.07)
 
-#-----------Création de la page-----------------------
 
+
+#-----Accueil------CREATION PAGE D'ACCUEIL-----------------------
 
 def page_accueil():
     global root
@@ -143,17 +164,17 @@ def page_accueil():
     root.config(background="#AED6F1")  # choisir un code couleur hexadecimal (rouge : #FF0000 / bleu : #0000FF / rose : #FFC0CB)
     root.resizable(width=True, height=True)  # autorisation de modification de la taille de la fenêtre
 
-    texte1 = tk.Label(root, text="Bienvenue dans la partie !\n Vous êtes prêts ?", font=("Courrier", 20), bg="#D6EAF8", fg="black")
-    texte1.place(relx=0.330, rely=0.050, width=400, height=100)
+    l2 = tk.Label(root, text="Bienvenue dans la partie !\n Vous êtes prêts ?", font=("Courrier", 20), bg="#D6EAF8", fg="black")
+    l2.place(relx=0.350, rely=0.050, relwidth=0.35, relheight=0.2)
 
-    bouton1 = tk.Button(root, text="Editer", width=10,font="Courrier 20", bg='#5499C7', fg='white', command=lambda: [root.quit, menu_edition()])
-    bouton1.place(relx=0.300, rely=0.400)
+    b_editer = tk.Button(root, text="Editer", width=10,font="Courrier 20", bg='#5499C7', fg='white', command=lambda: [root.quit, menu_edition()])
+    b_editer.place(relx=0.270, rely=0.400, relwidth=0.2, relheight=0.1)
 
-    bouton1bis = tk.Button(root, text="Jouer", width=10,font="Courrier 20", bg='#5499C7', fg='white', command=lambda: [root.quit, menu_jeu()])
-    bouton1bis.place(relx=0.570, rely=0.400)
+    b_jouer = tk.Button(root, text="Jouer", width=10,font="Courrier 20", bg='#5499C7', fg='white', command=lambda: [root.quit, menu_jeu()])
+    b_jouer.place(relx=0.570, rely=0.400, relwidth=0.2, relheight=0.1)
 
-    bouton2 = tk.Button(root, text="Quitter", width=10,font="Courrier", bg='white', fg='red', command=quit)
-    bouton2.place(relx=0.450, rely=0.800)
+    b_quitter = tk.Button(root, text="Quitter", width=10,font="Courrier", bg='white', fg='red', command=quit)
+    b_quitter.place(relx=0.450, rely=0.800,  relwidth=0.15, relheight=0.07)
 
     bar_menu()
 
